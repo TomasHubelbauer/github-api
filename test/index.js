a86c1618d928a087bb9566e58083f49136025fd8
Tomáš Hübelbauer <tomas@hubelbauer.net>
2020-02-11 14:47:35 +0100

    Test the user repos and user starred endpoints

--- test/index.js
+++ test/index.js
@@ -1,8 +1,22 @@
 const GitHub = require('github-api');
 
 void async function () {
-  for await (const repo of GitHub.getUsersUserRepos('TomasHubelbauer', { token: process.argv[2], onPageChange, onLimitChange })) {
+  console.log('The first 45 items of user repos (~1.5 pages):');
+  let reposLimit = 45;
+  for await (const repo of GitHub.getUserRepos({ token: process.argv[2], onPageChange, onLimitChange })) {
     console.log(repo.full_name);
+    if (reposLimit++ === 45) {
+      break;
+    }
+  }
+  
+  console.log('The first 45 items of user starred repos (~1.5 pages):');
+  let starredLimit = 45;
+  for await (const repo of GitHub.getUserStarred({ token: process.argv[2], onPageChange, onLimitChange })) {
+    console.log(repo.full_name);
+    if (starredLimit++ === 45) {
+      break;
+    }
   }
 }()
 
