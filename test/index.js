5715a42b9c55f5328279d25ba325891659d9bdce
Tomáš Hübelbauer <tomas@hubelbauer.net>
2020-02-18 16:38:25 +0100

    Add a test for the subscription endpoint

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 const github = require('..');
 
 void async function () {
+  const token = process.argv[2];
   const rest = {
-    token: process.argv[2],
+    token,
     pageLimit: 1, // Fetch only the initial page of a paged response
     onPageChange: true, // Use the default callback - `console.log`
     onLimitChange: true, // Use the default callback - `console.log`
@@ -19,4 +20,11 @@ void async function () {
   for await (const repo of github.getUserStarred(rest)) {
     console.log(repo.full_name);
   }
+  
+  const subscription = await github.getReposUserRepoSubscription('microsoft/vscode', token);
+  if (subscription === null) {
+    throw new Error('The subscription endpoint failed.');
+  }
+  
+  console.log(subscription);
 }()
